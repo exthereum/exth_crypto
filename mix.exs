fd897317ac7d35deb954f2190d9f40b909effb00
Geoffrey Hayes <hayesgm@gmail.com>
2018-08-20 11:43:32 -0700

    Bump libsecp256k1 dep and bump to 0.1.6
    This patch bumps to the new version of libsecp256k1 which should fix app errors, as well as issues with `git ls-files`.

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule ExthCrypto.Mixfile do
 
   def project do
     [app: :exth_crypto,
-     version: "0.1.5",
+     version: "0.1.6",
      elixir: "~> 1.4",
      description: "Exthereum's Crypto Suite.",
       package: [
@@ -35,7 +35,7 @@ defmodule ExthCrypto.Mixfile do
   # Type "mix help deps" for more examples and options
   defp deps do
     [
-      {:libsecp256k1, "~> 0.1.7", app: false},
+      {:libsecp256k1, "~> 0.1.9"},
       {:keccakf1600, "~> 2.0.0", hex: :keccakf1600_orig},
       {:credo, "~> 0.8", only: [:dev, :test], runtime: false},
       {:ex_doc, "~> 0.17", only: :dev, runtime: false},
